d9803f7eaedd5a9ee044d479519ee18930363c05
Owen Stranathan <owen+github@appfigures.com>
2019-02-03 21:18:14 -0500

    Refactor ship combination code

---
+++ Assets/Scripts/Nozzle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nozzle : MonoBehaviour, IShipPart
+{
+    public void Combine(Ship ship)
+    {
+        ship.attachNozzle(this);
+    }
+}
