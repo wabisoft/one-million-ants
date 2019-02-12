25f4a4a733d7589ffe189a566965a9121565d34b
Owen Stranathan <owenstranathan@gmail.com>
2019-02-11 23:45:01 -0500

    testing testing.... and playing. Playing

--- Assets/Scripts/Common/DragNDrop.cs
+++ Assets/Scripts/Common/DragNDrop.cs
@@ -8,11 +8,13 @@ public class DragNDrop : MonoBehaviour
     public float HeightMultiplier = 1.05f;
     private Camera _camera {get {return Camera.main; }}
     private Rigidbody _rigidbody { get { return GetComponent<Rigidbody>(); } } 
+    private Vehicle _vehicle { get { return GetComponent<Vehicle>(); } }
     private Gravity _gravity { get { return GetComponent<Gravity>(); } }
     private Vector3 _up { get { return transform.position - _planet.transform.position; } }
     private Vector3 _axis;
     private float _theta;
     private Planet _planet;
+    private Vector3 _previousPos;
 
     void OnMouseDown()
     {
@@ -20,33 +22,37 @@ public class DragNDrop : MonoBehaviour
         _gravity.on = false;
         transform.position += _up.normalized * HeightMultiplier;
         _rigidbody.velocity = Vector3.zero;
+        _vehicle.Steer = false;
+    }
+    
+    private RaycastHit? GetPlanetHit()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000)) {
+            if (hit.collider as SphereCollider == _planet.Sphere) { return hit; }
+        }
+        return null;
     }
 
     void OnMouseDrag()
     {
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000))
-        {
-            if (hit.collider == GetComponent<Collider>())
-            {
-                return;
-            }
-            else {
-                var _hitPointRelVec = hit.point - _planet.transform.position;
-                _axis = Vector3.Cross(_up, _hitPointRelVec);
-                _theta = Vector3.SignedAngle(_up, _hitPointRelVec, _axis);
-                transform.RotateAround(_planet.transform.position, _axis, _theta);
-            } 
+        var hit = GetPlanetHit();
+        if (hit.HasValue) {
+            _previousPos = hit.Value.point;
+            var _hitPointRelVec = hit.Value.point - _planet.transform.position;
+            _axis = Vector3.Cross(_up, _hitPointRelVec);
+            _theta = Vector3.SignedAngle(_up, _hitPointRelVec, _axis);
+            transform.RotateAround(_planet.transform.position, _axis, _theta);
         }
     }
 
     void OnMouseUp()
     {
-
         _gravity.on = true;
         var tangentialVelocity = Vector3.Cross(_axis, transform.position - _planet.transform.position);
         _rigidbody.velocity = Vector3.ClampMagnitude(tangentialVelocity, _planet.OrbitalVelocity);
+        _vehicle.Steer = true;
     }
 
 }
--- Assets/Scripts/Common/Vehicle.cs
+++ Assets/Scripts/Common/Vehicle.cs
@@ -3,40 +3,38 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-
-public interface IVehicle
-{
-    void Seek(Vector3 target);
-    void Arrive(Vector3 target);
-    void Path();
-}
-
 [RequireComponent(typeof(Rigidbody))]
-public class Vehicle : MonoBehaviour, IVehicle
+public class Vehicle : MonoBehaviour
 {
 
     public float MaxSpeed = 3f; // Walking speed
-    public Planet Planet;
     public float PathRadius = .05f;
     public int PathVertices = 100;
+    public bool Steer = true;
     public ComputePath PathDelegate = Utilities.ComputeSpiralPath;
     private Vector3[] _path;
     private Rigidbody _rigidbody { get { return GetComponent<Rigidbody>();  } }
-    private Vector3 _steering;
-
+    protected Planet _planet;
+ 
 
     void Start()
     {
+        _planet = Utilities.SelectPlanet(gameObject);
         _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         _rigidbody.useGravity = false;
         _path = new Vector3[PathVertices];
-        PathDelegate(ref _path, Planet.Sphere.center, Planet.Radius);
+        PathDelegate(ref _path, _planet.Sphere.center, _planet.Radius);
         // pathDelegate(ref _path, new Vector3(-4.53f, 0.1f, -4.74f), 8.0f);
+        DebugPath();
     }
 
     // pass the canvasbounds -- bounds are hard coded
     void FixedUpdate()
     {
+        if (!Steer) {
+            _rigidbody.velocity = Vector3.zero;
+            return;
+        }
 #if DEBUG
         for (int i = 1; i < PathVertices; i++)
             Debug.DrawLine(_path[i - 1], _path[i], Color.red);
@@ -47,14 +45,14 @@ public class Vehicle : MonoBehaviour, IVehicle
             _rigidbody.velocity += _rigidbody.transform.forward * MaxSpeed / 4.0f;
         }
         Path();
-        _rigidbody.velocity += _steering;
         _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, MaxSpeed);
     }
 
     public void Seek(Vector3 target)
     {
         var desire = (transform.position - target).normalized * MaxSpeed;
-        _steering = _rigidbody.velocity - desire;
+        var _steering = _rigidbody.velocity - desire;
+        _rigidbody.velocity += _steering;
 #if DEBUG
         Debug.DrawLine(transform.position, target, Color.black);
         Debug.DrawLine(transform.position, transform.position + _steering * 2, Color.red);
@@ -70,7 +68,8 @@ public class Vehicle : MonoBehaviour, IVehicle
         var ramped_speed = MaxSpeed * (distance / PathRadius);
         var clipped_speed = Mathf.Min(ramped_speed, MaxSpeed);
         var desired_velocity = (clipped_speed / distance) * target_offset;
-        _steering = desired_velocity - _rigidbody.velocity;
+        var _steering = desired_velocity - _rigidbody.velocity;
+        _rigidbody.velocity += _steering;
     }
 
     public void Path()
@@ -85,7 +84,7 @@ public class Vehicle : MonoBehaviour, IVehicle
                     /   |
                    a----o---->b
                    |----|\     ab = b - a
-                       \   o = a + (s * normalize(ab))
+                       \   o = a + (s * normalize(ab)) or a + Project(ap, ab)
                         s = dot(ap, normalize(ab)
         */
         // float dt = Time.deltaTime * dtCoefficient;
@@ -96,17 +95,15 @@ public class Vehicle : MonoBehaviour, IVehicle
         var b = pathSeg[1];
         var ab = b - a;
         var ap = p - a;
-        var s = Vector3.Dot(ap, ab.normalized);
-        var o = a + (s * ab.normalized);
+        var o = a + Vector3.Project(ap, ab); // TEST TEST TEST
+        //var s = Vector3.Dot(ap, ab.normalized);
+        //var o = a + (s * ab.normalized);
         var e = Vector3.Distance(p, o);
-        if (e >= PathRadius)
-        {
-            if (a == b)
-            {
+        if (e >= PathRadius) {
+            if (a == b) {
                 Arrive(a);
             }
-            else
-            {
+            else {
                 var d = o + ab.normalized;
                 Seek(d);
             }
@@ -117,35 +114,24 @@ public class Vehicle : MonoBehaviour, IVehicle
     {
         var min = float.MaxValue;
         var index = 0;
-        for (var i = 0; i < _path.Length; i++)
-        {
-            var dist = (point - _path[i]).sqrMagnitude; //optimization (sqrt is expensive);
-            // var dist = Vector3.Distance(point, _path[i]);
-            if (dist < min)
-            {
+        for (var i = 0; i < _path.Length; i++) {
+            var dist = (point - _path[i]).sqrMagnitude; //optimization (sqrt is expensive)
+            if (dist < min) {
                 index = i;
                 min = dist;
             }
         }
-        if (index + 1 >= _path.Length)
-        {
+        if (index + 1 >= _path.Length) {
             return new Vector3[] { _path[index], _path[index] };
         }
         return new Vector3[] { _path[index], _path[index + 1] };
     }
 
-    void OnDrawGizmos()
+    void DebugPath()
     {
-        Vector3[] path = new Vector3[PathVertices];
-        // pathDelegate(ref path, new Vector3(-4.53f, 0.1f, -4.74f), 8.0f);
-        PathDelegate(ref path, Planet.Sphere.center, Planet.Radius);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(path[0], 0.05f);
-        Gizmos.color = Color.red;
-        for (int i = 1; i < path.Length; i++)
+        for (int i = 1; i < _path.Length; i++)
         {
-            Gizmos.DrawSphere(path[i], 0.05f);
-            Debug.DrawLine(path[i - 1], path[i], Color.red);
+            Debug.DrawLine(_path[i - 1], _path[i], Color.red);
         }
     }
 
