efcd02374db265fe50708543e2a1be5004297060
Stranathan <ian.p.stranathan@gmail.com>
2019-04-04 16:29:50 -0700

    something

---
+++ Assets/Materials/gemBump.shader
@@ -0,0 +1,50 @@
+﻿Shader "Custom/gemBump"
+{
+	Properties
+	{
+		_DiffuseTex("Diffuse Texture", 2D) = "white" {}
+		_BumpTex("Bump Texture", 2D) = "bump" {}
+		_bumpMultiplier("Bump Amount", Range(0,20)) = 1
+		_brightMultiplier("Brightness", Range(0,10)) = 1
+		_Emission("Emissive Color", Color) = (1,1,1,1)
+		_Metallic("Metallic", Range(0,1)) = 0.0
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+	}
+		SubShader
+		{
+
+			Tags { "RenderType" = "Opaque" }
+			LOD 200
+
+			CGPROGRAM
+			// Physically based Standard lighting model, and enable shadows on all light types
+			#pragma surface surf Standard fullforwardshadows
+
+			sampler2D _DiffuseTex;
+			sampler2D _BumpTex;
+			half _bumpMultiplier;
+			half _brightMultiplier;
+			fixed4 _Emission;
+			half _Metallic;
+			half _Glossiness;
+
+			struct Input {
+				float3 viewDir;
+				float2 uv_DiffuseTex;
+				float2 uv_BumpTex;
+			};
+
+			void surf(Input IN, inout SurfaceOutputStandard o) {
+				half surfViewDotProd = dot(normalize(IN.viewDir), normalize(o.Normal));
+				o.Albedo = tex2D(_DiffuseTex, IN.uv_DiffuseTex).rgb;
+				o.Normal = UnpackNormal(tex2D(_BumpTex, IN.uv_BumpTex)) * _brightMultiplier;
+				o.Normal *= float3(_bumpMultiplier,_bumpMultiplier,1);
+				o.Emission = _Emission.rgb * .5 * surfViewDotProd;
+				o.Metallic = _Metallic;
+				o.Smoothness = _Glossiness;
+			}
+
+		  ENDCG
+		}
+			Fallback "Diffuse"
+}
---
+++ Assets/Scripts/SphericalPath.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphericalPath : MonoBehaviour
+{
+
+    SphereCollider myCollider;
+    private float _radius;
+    public Vector3 testPoint;
+    public float deltaTheta = Mathf.PI/10.0f; // 10 points
+    Vector3[] points;
+
+    void Start()
+    {
+        myCollider = transform.GetComponent<SphereCollider>();
+        _radius = myCollider.radius;
+        testPoint = new Vector3(0.0f, 0.0f, _radius);
+        points = new Vector3[10];
+        makePoints(testPoint);
+    }
+
+    void makePoints(Vector3 somePoint)
+    {
+        // non signed angle between y-axis and point
+        float theta = Vector3.Angle(somePoint, Vector3.up);
+        theta *= Mathf.Deg2Rad;
+        // const angle on x-z plane
+        float phi = Mathf.Atan(somePoint.x / somePoint.z);
+        phi *= Mathf.Deg2Rad;
+
+        //Debug.Log("Theta equals: " + theta);
+        //Debug.Log("Phi equals: " + phi);
+        //Debug.Log("Radius equals: " + _radius);
+
+        // if you want to always end at pole: theta --> 0
+        // while(theta > 0.0f)
+        //{
+        //}
+        for (int i = 0; i < 10; i++)
+        {
+            float xx = _radius * Mathf.Sin(theta) * Mathf.Cos(phi);
+            float zz = _radius * Mathf.Sin(theta) * Mathf.Sin(phi);
+            float yy = _radius * Mathf.Cos(theta);
+
+            Vector3 point = new Vector3(xx, yy, zz);
+            points[i] = point;
+            //Debug.Log(point.ToString());
+            theta -= deltaTheta;
+        }
+     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (points != null)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                //Debug.Log("hmm");
+                Gizmos.DrawSphere(points[i], 0.1f);
+            }
+        }
+    }
+}
